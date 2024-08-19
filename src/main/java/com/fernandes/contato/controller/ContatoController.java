7e236840c687174f2fe1615f7c04eeb3b1f9b110
GustavoSFer <gustavo.sfernander@gmail.com>
2024-08-19 16:25:07 -0300

    Feat: Colocando o spring para controlar o controller e o service, e colocando o endereço no controller

--- src/main/java/com/fernandes/contato/controller/ContatoController.java
+++ src/main/java/com/fernandes/contato/controller/ContatoController.java
@@ -1,9 +1,13 @@
 package com.fernandes.contato.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fernandes.contato.service.ContatoService;
 
+@RestController
+@RequestMapping(value = "/Contato")
 public class ContatoController {
 	
 	@Autowired
--- src/main/java/com/fernandes/contato/service/ContatoService.java
+++ src/main/java/com/fernandes/contato/service/ContatoService.java
@@ -1,9 +1,11 @@
 package com.fernandes.contato.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.fernandes.contato.repository.IContato;
 
+@Service
 public class ContatoService {
 	
 	@Autowired
