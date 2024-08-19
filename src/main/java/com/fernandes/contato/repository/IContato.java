cbee3d4da18ac5eac55e60686bce08a6691e0133
GustavoSFer <gustavo.sfernander@gmail.com>
2024-08-19 16:18:17 -0300

    Feat: Colocando o spring para controlar o repository

--- src/main/java/com/fernandes/contato/repository/IContato.java
+++ src/main/java/com/fernandes/contato/repository/IContato.java
@@ -1,9 +1,11 @@
 package com.fernandes.contato.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fernandes.contato.entities.Contato;
 
+@Repository
 public interface IContato extends JpaRepository<Contato, Integer> {
 
 }
