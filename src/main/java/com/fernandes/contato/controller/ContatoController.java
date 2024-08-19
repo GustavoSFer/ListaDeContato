be16d2870f2ea5f909d4e5e36b8778b9bb8ad320
GustavoSFer <gustavo.sfernander@gmail.com>
2024-08-19 16:42:36 -0300

    Feat: Create - fluxo de criação de um novo contato

--- src/main/java/com/fernandes/contato/controller/ContatoController.java
+++ src/main/java/com/fernandes/contato/controller/ContatoController.java
@@ -1,9 +1,13 @@
 package com.fernandes.contato.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fernandes.contato.entities.Contato;
 import com.fernandes.contato.service.ContatoService;
 
 @RestController
@@ -12,5 +16,12 @@ public class ContatoController {
 	
 	@Autowired
 	ContatoService contatoService;
+	
+	@PostMapping
+	public ResponseEntity<Contato> createContato(@RequestBody Contato contato) {
+		Contato contatoSalvo = contatoService.createContato(contato);
+		
+		return ResponseEntity.ok().body(contatoSalvo);
+	}
 
 }
--- src/main/java/com/fernandes/contato/service/ContatoService.java
+++ src/main/java/com/fernandes/contato/service/ContatoService.java
@@ -3,6 +3,7 @@ package com.fernandes.contato.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fernandes.contato.entities.Contato;
 import com.fernandes.contato.repository.IContato;
 
 @Service
@@ -10,5 +11,9 @@ public class ContatoService {
 	
 	@Autowired
 	IContato contatoRepository;
+	
+	public Contato createContato(Contato contato) {
+		return contatoRepository.save(contato);
+	}
 
 }
