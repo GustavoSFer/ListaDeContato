be096ab3215318dc4d97ca2dfb97b3721510d725
GustavoSFer <gustavo.sfernander@gmail.com>
2024-08-19 16:00:45 -0300

    Feat: Colocando o spring para controlar a classe contato

--- src/main/java/com/fernandes/contato/entities/Contato.java
+++ src/main/java/com/fernandes/contato/entities/Contato.java
@@ -5,10 +5,18 @@ import java.util.Objects;
 
 import com.fernandes.contato.enums.TipoCelular;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Contato implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String cpf;
