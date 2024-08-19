7b57534719306007f80903b8c298973e96917fdf
GustavoSFer <gustavo.sfernander@gmail.com>
2024-08-19 16:22:41 -0300

    Feat: Criado o controller ou resource

---
+++ src/main/java/com/fernandes/contato/controller/ContatoController.java
@@ -0,0 +1,12 @@
+package com.fernandes.contato.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fernandes.contato.service.ContatoService;
+
+public class ContatoController {
+	
+	@Autowired
+	ContatoService contatoService;
+
+}
