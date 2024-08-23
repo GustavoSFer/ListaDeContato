af0ae502bd4ad23bbd1e47d18ca2d3365b566447
GustavoSFer <gustavo.sfernander@gmail.com>
2024-08-23 14:37:28 -0300

    Feat: Listando todos os contatos

--- src/main/java/com/fernandes/contato/controller/ContatoController.java
+++ src/main/java/com/fernandes/contato/controller/ContatoController.java
@@ -1,7 +1,10 @@
 package com.fernandes.contato.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,4 +27,10 @@ public class ContatoController {
 		return ResponseEntity.ok().body(contatoSalvo);
 	}
 
+	@GetMapping
+	public ResponseEntity<List<Contato>> findAll() {
+		List<Contato> contatos = contatoService.findAll();
+		
+		return ResponseEntity.ok().body(contatos);
+	}
 }
--- src/main/java/com/fernandes/contato/service/ContatoService.java
+++ src/main/java/com/fernandes/contato/service/ContatoService.java
@@ -1,5 +1,7 @@
 package com.fernandes.contato.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +18,9 @@ public class ContatoService {
 		return contatoRepository.save(contato);
 	}
 
+	public List<Contato> findAll() {
+		List<Contato> contatos = contatoRepository.findAll();
+		
+		return contatos;
+	}
 }
